2a478e473c662517b6ff24ae9b7d9ed44bab8b3d
Nanees Adel <naneesadel15@gmail.com>
2024-05-04 17:56:19 +0300

    first_upload

--- app/src/main/java/com/example/agent/MainActivity.java
+++ app/src/main/java/com/example/agent/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.agent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Delay for 5 seconds
+        int delayMillis = 5000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the other activity
+                startActivity(new Intent(MainActivity.this, Login_Activity.class));
+                // Finish the current activity
+                finish();
+            }
+        }, delayMillis);
     }
 }
