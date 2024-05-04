273b1bec30206b275024c942d01b0d4867c28d4a
Ofaa85 <youssefroaf94@gmail.com>
2024-05-04 20:52:54 +0300

    Adding Arabic In Voice

--- app/src/main/java/com/example/agent/Activity_Report_Details.java
+++ app/src/main/java/com/example/agent/Activity_Report_Details.java
@@ -40,7 +40,7 @@ public class Activity_Report_Details extends AppCompatActivity {
 
         Intent intent= new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ar-EG,en-US");
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Hi Speak");
 
         try {
